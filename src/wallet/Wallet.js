8962bdc7e53bfdb957820f422f4868c652382dd5
Thaymer portillo <42485564+thaymerapv@users.noreply.github.com>
2021-01-03 15:19:25 +0100

    add Wallet class

---
+++ src/wallet/Wallet.js
@@ -0,0 +1,23 @@
+import Elliptic from 'elliptic';
+
+// eslint-disable-next-line new-cap
+const ec = new Elliptic.ec('secp256k1');
+
+const INITIAL_BALANCE = 100;
+
+class Wallet {
+  constructor() {
+    this.balance = INITIAL_BALANCE;
+    this.keyPair = ec.genKeyPair();
+    this.publickKey = this.keyPair.getPublic().encode('hex');
+  }
+
+  toString() {
+    const { balance, publicKey } = this;
+    return `Wallet -
+    publicKey    :${publicKey}
+    balance       :${balance}`;
+  }
+}
+
+export default Wallet;
