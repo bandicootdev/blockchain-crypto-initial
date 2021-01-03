7ae6f828398183d9b92fe7cbb0dcd091ed11d3c2
Thaymer portillo <42485564+thaymerapv@users.noreply.github.com>
2021-01-03 19:00:03 +0100

    add test for memoryPool class

---
+++ src/test/memoryPool.test.js
@@ -0,0 +1,34 @@
+import MemoryPool from '../blockchain/memoryPool';
+import Wallet, { Transaction } from '../wallet';
+
+describe('MemoryPool', () => {
+  let memoryPool;
+  let wallet;
+  let transaction;
+
+  beforeEach(() => {
+    memoryPool = new MemoryPool();
+    wallet = new Wallet();
+    transaction = Transaction.create(wallet, 'random-address', 5);
+    memoryPool.adOrUpdate(transaction);
+  });
+
+  it('has one transaction ', () => {
+    expect(memoryPool.transactions.length).toEqual(1);
+  });
+
+  it('add a transaction to the memoryPool ', () => {
+    const found = memoryPool.transactions.find(({ id }) => id === transaction.id);
+    expect(found).toEqual(transaction);
+  });
+
+  it('update a transaction in the memoryPool ', () => {
+    const txOld = JSON.stringify(transaction);
+    const txNew = transaction.update(wallet, 'other-addres', 10);
+    memoryPool.adOrUpdate(txNew);
+    expect(memoryPool.transactions.length).toEqual(1);
+    const found = memoryPool.transactions.find(({ id }) => id === transaction.id);
+    expect(JSON.stringify(found)).not.toEqual(txOld);
+    expect(txNew).toEqual(found);
+  });
+});
---
+++ src/wallet/index.js
@@ -0,0 +1,5 @@
+import Wallet from './Wallet';
+import Transaction from './Transaction';
+
+export { Transaction };
+export default Wallet;
