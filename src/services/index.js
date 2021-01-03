bc7367d9d6c4b6989104e97c66521251215a11fd
Thaymer portillo <42485564+thaymerapv@users.noreply.github.com>
2021-01-03 05:48:27 +0100

    little scaffolding from endpoint the server express

--- src/services/index.js
+++ src/services/index.js
@@ -10,9 +10,20 @@ const blockchain = new Blockchain();
 blockchain.addBlock('express');
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/blocks', (req, res) => {
   res.status(200).json(blockchain.blocks);
+});
+
+app.post('/mine', (req, res) => {
+  const { body: { data } } = req;
+  const block = blockchain.addBlock(data);
+
+  res.status(200).json({
+    blocks: blockchain.blocks.length,
+    block,
+  });
 });
 
 app.listen(HTTP_PORT, () => console.log(`server on port ${HTTP_PORT}`));
