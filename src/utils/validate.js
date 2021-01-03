19ddcdc6acb5d156bd0dfef0c466963accc1bc88
Thaymer portillo <42485564+thaymerapv@users.noreply.github.com>
2021-01-03 04:15:08 +0100

    adding validate hash blockchain

---
+++ src/utils/validate.js
@@ -0,0 +1,16 @@
+import Block from '../blockchain/Block';
+
+export default (blockchain) => {
+  const [genesisBlock, ...blocks] = blockchain;
+  if (JSON.stringify(genesisBlock) !== JSON.stringify(Block.genesis)) throw Error('invalid Genesis Block');
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < blocks.length; i++) {
+    const {
+      previousHash, timestamp, hash, data,
+    } = blocks[i];
+    const previousBlock = blockchain[i];
+    if (previousHash !== previousBlock.hash) throw Error('invalid previous Hash');
+    if (hash !== Block.hash(timestamp, previousHash, data)) throw Error('invalid Hash');
+  }
+  return true;
+};
